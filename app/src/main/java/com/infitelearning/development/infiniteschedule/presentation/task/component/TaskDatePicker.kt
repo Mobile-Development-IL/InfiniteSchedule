9f81b6342093137292a2f284655c49c2bfef5ced
rahmad <rajaikhsan2021@gmail.com>
2024-05-27 09:46:36 +0700

    Initial commit

--- app/src/main/java/com/infitelearning/development/infiniteschedule/presentation/task/component/TaskDatePicker.kt
+++ app/src/main/java/com/infitelearning/development/infiniteschedule/presentation/task/component/TaskDatePicker.kt
@@ -1,7 +1,6 @@
 package com.infitelearning.development.infiniteschedule.presentation.task.component
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
@@ -9,7 +8,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.DialogProperties
 import java.time.Instant
 import java.time.LocalDate
